6470086520fc4432dd71f9f8c17a4e20cc1bf9c7
pupungeggang2 <kyongkyong2892@gmail.com>
2026-08-20 14:04:08 +0900

    started puzzle

---
+++ puzzle/src/Game.cs
@@ -0,0 +1,24 @@
+namespace Puzzle;
+
+public class Game
+{
+    public Game()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void Loop()
+    {
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
---
+++ puzzle/src/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Puzzle;
+
+public class Program
+{
+    public static void Main()
+    {
+        Game game = new Game();
+        game.Run();
+        game.Dispose();
+    }
+}
